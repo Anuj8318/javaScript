dff3110cbdcaf64fb0890fc730990346a16ebcc4
Anuj kumar singh <114329800+Anuj8318@users.noreply.github.com>
2024-03-05 13:47:19 +0000

    conversion of datatype

---
+++ 01_basic/03_datatypeConversion.js
@@ -0,0 +1,33 @@
+let score = "33abs"
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "22abs" => Nan
+// true => 1;
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn  = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+// 1 => true ; 0=> false
+// "" =>false
+
+//"anuj" =>true
+
+
+let num = 33
+
+let stringNumber = String(num)
+
+console.log(stringNumber)
+
+console.log( typeof stringNumber)
