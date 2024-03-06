b7de44a9cfb2947f5b72625780b3cf67721e8e10
Anuj kumar singh <114329800+Anuj8318@users.noreply.github.com>
2024-03-06 13:59:44 +0000

    datype summary

---
+++ 01_basic/04_comparsion.js
@@ -0,0 +1,16 @@
+console.log("2">1);
+
+// try to avoid these type of conversion 
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
+
+
+// strict check 
+console.log("2"==2);
---
+++ 01_basic/dataytpeSummary.js
@@ -0,0 +1,35 @@
+// primitive datatype
+//  this is the type of the data which  is copy my value not copy by reference 
+
+// there is 7 type of primitive data type :
+// String, Number , Boolean , null, undefined ,Symbol(for unique),BigInt
+
+
+const score = 100
+const scoreValue = 100.3  
+
+const id = Symbol('123')
+
+// const bigNumber = 3442525324352n; this is used to decalare the bigInt datatype
+
+
+
+// NON primitive datatype (Reference)
+
+// Array, Objects, Functions
+
+// 1.Array
+const heros = ["anuj", "suraj" ,"ramesh"];
+
+
+// 2. Object 
+let myObj = {
+    name: "anuj",
+    age :22,
+}
+
+// 3. Functions
+const myFunction = function(){
+    console.log("hello world");
+}
+
